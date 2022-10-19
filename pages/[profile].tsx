b456f5566ad89b6cf5f5f9cba123c74360f2acb3
김지호 <crsn1111@gmail.com>
2022-10-19 20:54:28 +0900

    [feat] dynamic page ssg

---
+++ pages/[profile].tsx
@@ -0,0 +1,79 @@
+import { dehydrate } from "@tanstack/react-query";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import Seo from "../components/common/Seo";
+import instance from "../core/api/axios";
+import {
+  getProfileDetail,
+  useGetProfileDetail,
+} from "../core/api/profile/queries";
+import { ProfileProps } from "../core/api/profile/types";
+import { InfoWrapper, ProfileContainer } from "../styles/Profile";
+import { queryClient } from "./_app";
+
+type Profile = {
+  profile: string;
+};
+
+interface PageProps {
+  params: Profile;
+}
+
+const Profile = () => {
+  const {
+    query: { profile },
+  } = useRouter();
+  const { data: MyInfo } = useGetProfileDetail(String(profile));
+  if (MyInfo === undefined) return;
+  return (
+    <ProfileContainer>
+      <Seo title={`Profile / ${String(profile)}`} />
+      <InfoWrapper>
+        <Image
+          src={MyInfo.avatar}
+          alt={MyInfo.avatar}
+          width={256}
+          height={256}
+          layout="fixed"
+        />
+        <span>{MyInfo.name}</span>
+      </InfoWrapper>
+    </ProfileContainer>
+  );
+};
+
+export default Profile;
+
+export const getStaticPaths = async () => {
+  const res = await instance.get("/profile");
+  return {
+    paths: res.data.map((data: ProfileProps) => {
+      return {
+        params: {
+          profile: data.id,
+        },
+      };
+    }),
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context: PageProps) => {
+  console.log(context);
+  try {
+    await queryClient.fetchQuery(["profile", context.params.profile], () =>
+      getProfileDetail(context.params.profile)
+    );
+    return {
+      props: {
+        dehydratedState: dehydrate(queryClient),
+      },
+    };
+  } catch (error) {
+    return {
+      notFound: true,
+    };
+  } finally {
+    queryClient.clear();
+  }
+};
